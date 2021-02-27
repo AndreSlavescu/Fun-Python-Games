50c0598b98bcef0d0b64edfa7e0404ca46d94b90
AndreSlavescu <51034490+AndreSlavescu@users.noreply.github.com>
2021-02-27 11:58:08 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,16 +1,26 @@
 import random
 
-youC = 0
-oppC = 0
+youC = 0 #my points
+oppC = 0 #opponent points
+gameC = 0 #number of games played
 continuegame = True
 
 while continuegame:
-  continueplay = input("enter play to continue or stop to stop: ")
-  if continueplay == "play":
+  continueplay = input("Please enter 'Play' if you want to play the game, or 'Stop' to stop: ")
+  while continueplay not in ["Stop", "stop", "Play", "play"]:
+    print("Wrong input, please enter either 'Play' or 'Stop'")
+    continueplay = input("Please enter 'Play' if you want to play the game, or 'Stop' to stop: ")
+  if continueplay == "play" or continueplay == "Play":
     pass
-  else:
+  elif continueplay == "stop" or continueplay == "Stop":
     break
-  opponent = input("input either S,P, or R: ")
+
+  gameC += 1
+  print(f"Game {gameC} Rock, Paper, Scissors - Play!")
+  opponent = input("Choose your weapon: R for Rock, P for Paper, or S for Scissors: ")
+  while opponent not in ["S", "s", "P", "p", "R", "r"]:
+    print("Wrong input, please enter either 'R', 'P', or 'S'")
+    opponent = input("Choose your weapon: R for Rock, P for Paper, or S for Scissors: ")
   you = random.randint(0,2)
   
   if you == 0:
@@ -21,24 +31,27 @@ while continuegame:
     print("I chose: S")
   print(f"You chose: {opponent}")
 
-  if you == 0 and opponent == "S":
+  if you == 0 and (opponent == "S" or opponent == "s"):
     youC += 1
-  elif you == 0 and opponent == "P":
+  elif you == 0 and (opponent == "P" or opponent == "p"):
     oppC += 1
-  elif you == 1 and opponent == "R":
+  elif you == 1 and (opponent == "R" or opponent == "r"):
     youC += 1
-  elif you == 1 and opponent == "S":
+  elif you == 1 and (opponent == "S" or opponent == "s"):
     oppC += 1
-  elif you == 2 and opponent == "P":
+  elif you == 2 and (opponent == "P" or opponent == "p"):
     youC += 1
-  elif you == 2 and opponent == "R":
+  elif you == 2 and (opponent == "R" or opponent == "r"):
     oppC += 1
    
 diff = youC - oppC
 
+print("~~~Final Scores~~~")
+print(f"Your Score: {oppC},  My Score: {youC}")
+
 if diff > 0:
-  print(f"you win by {diff} games")
+  print(f"Congratulations! You outplayed me by {diff} games!")
 elif diff < 0:
-  print(f"you lose by {abs(diff)} games")
+  print(f"You're a great opponent! I beat you by only {abs(diff)} games.")
 else:
-  print(f"We're all tied up with {youC} games")
+  print(f"We're all tied up with {youC} games each. Let's play again soon.")
